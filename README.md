a9de786d75f42c3e0f43fc44e9a4607ba301c8b8
Jesusprz24 <perez.jesus1039@gmial.com>
2022-11-10 20:50:52 -0700

    added more

--- README.md
+++ README.md
@@ -19,4 +19,7 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 
+# DEPLOYED URL (THIS IS THE URL THAT WILL TAKE YOU TO THE LIVE PAGE)
+https://jesusprz24.github.io/workyoubloodygenerator/
+
 
--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Assignment code
 var generateBtn = document.querySelector("#generate"); 
 // Write password to the #password input
-//Added chars for all characters
-//Set password length to 8
+
+//Added character and put all letters, numbers and symbols inside
 function genPassword() {
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var passwordLength = 8;
   var password = "";
 
 for (var i = 0; i <= passwordLength; i++) {
- var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber +1); 
+ var randomNumber = Math.floor(Math.random() * characters.length);
+  password += characters.substring(randomNumber, randomNumber +1); 
 }
   document.getElementById("password").value = password;
 }
