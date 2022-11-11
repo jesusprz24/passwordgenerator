1672f7236b2a55f10df51fc7fb70d6ba4c05332f
Jesusprz24 <perez.jesus1039@gmial.com>
2022-11-10 20:20:25 -0700

    added finishing touches

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
+// Assignment code
+var generateBtn = document.querySelector("#generate"); 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Added chars for all characters
+//Set password length to 8
+function genPassword() {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 8;
+  var password = "";
 
-  passwordText.value = password;
-
+for (var i = 0; i <= passwordLength; i++) {
+ var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1); 
+}
+  document.getElementById("password").value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Replaced writePassword to genPassword to better be understood
+generateBtn.addEventListener("click", genPassword);
